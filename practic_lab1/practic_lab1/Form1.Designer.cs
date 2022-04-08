6df11975f2a08bddb153a80acb47c0b653cd83da
1ucif3r D3nnica <Fnatic4krimz@outlook.com>
2022-04-08 15:30:00 +0300

    fist_lab_complete!

--- practic_lab1/practic_lab1/Form1.Designer.cs
+++ practic_lab1/practic_lab1/Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -92,6 +93,7 @@
             this.button3.Size = new System.Drawing.Size(50, 50);
             this.button3.TabIndex = 4;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -100,6 +102,7 @@
             this.button4.Size = new System.Drawing.Size(50, 50);
             this.button4.TabIndex = 5;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -108,6 +111,7 @@
             this.button5.Size = new System.Drawing.Size(50, 50);
             this.button5.TabIndex = 6;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -116,6 +120,7 @@
             this.button6.Size = new System.Drawing.Size(50, 50);
             this.button6.TabIndex = 7;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -133,6 +138,7 @@
             this.button8.Size = new System.Drawing.Size(50, 50);
             this.button8.TabIndex = 9;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -141,6 +147,7 @@
             this.button9.Size = new System.Drawing.Size(50, 50);
             this.button9.TabIndex = 10;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -149,6 +156,7 @@
             this.button10.Size = new System.Drawing.Size(50, 50);
             this.button10.TabIndex = 11;
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // label2
             // 
@@ -183,12 +191,24 @@
             this.label4.Text = "количесвто бомб: 3";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(210, 490);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(162, 42);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Начать игру";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(575, 691);
+            this.Controls.Add(this.button11);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -207,6 +227,7 @@
             this.Font = new System.Drawing.Font("Mistral", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "practic_lab1_zadanie_19";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -231,6 +252,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button11;
     }
 }
 
--- practic_lab1/practic_lab1/Form1.cs
+++ practic_lab1/practic_lab1/Form1.cs
@@ -13,6 +13,7 @@ namespace practic_lab1
 {
     public partial class Form1 : Form
     {
+        public int[,] polei = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
         public Form1()
         {
             InitializeComponent();
@@ -40,13 +41,35 @@ namespace practic_lab1
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 2, 3);
+            if (perem == 7)
+            {
+                button7.Text = "BOOM!";
+                button7.BackColor = Color.Red;
+            }
+            else
+            {
+                button7.Text = perem.ToString();
+                button7.BackColor = Color.Blue;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            button2.Text = "1";
-            button2.BackColor = Color.Red;
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei , 1, 1);
+            if ( perem == 7)
+            {
+                button2.Text = "BOOM!";
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                button2.Text = perem.ToString();
+                button2.BackColor = Color.Blue;
+            }
+            
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -57,6 +80,213 @@ namespace practic_lab1
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+   
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            Saper.newgame(ref polei);
+            Saper.saper(ref polei);
+            button11.Text = "Начать сначала";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button10.Text = "";
+            button2.Text = "";
+            //--------------------------
+            button3.BackColor = Color.AntiqueWhite;
+            button4.BackColor = Color.AntiqueWhite;
+            button5.BackColor = Color.AntiqueWhite;
+            button6.BackColor = Color.AntiqueWhite;
+            button7.BackColor = Color.AntiqueWhite;
+            button8.BackColor = Color.AntiqueWhite;
+            button9.BackColor = Color.AntiqueWhite;
+            button10.BackColor = Color.AntiqueWhite;
+            button2.BackColor = Color.AntiqueWhite;
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 3, 3);
+            if (perem == 7)
+            {
+                button10.Text = "BOOM!";
+                button10.BackColor = Color.Red;
+            }
+            else
+            {
+                button10.Text = perem.ToString();
+                button10.BackColor = Color.Blue;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 1, 2);
+            if (perem == 7)
+            {
+                button3.Text = "BOOM!";
+                button3.BackColor = Color.Red;
+            }
+            else
+            {
+                button3.Text = perem.ToString();
+                button3.BackColor = Color.Blue;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 1, 3);
+            if (perem == 7)
+            {
+                button4.Text = "BOOM!";
+                button4.BackColor = Color.Red;
+            }
+            else
+            {
+                button4.Text = perem.ToString();
+                button4.BackColor = Color.Blue;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 2, 1);
+            if (perem == 7)
+            {
+                button5.Text = "BOOM!";
+                button5.BackColor = Color.Red;
+            }
+            else
+            {
+                button5.Text = perem.ToString();
+                button5.BackColor = Color.Blue;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 2, 2);
+            if (perem == 7)
+            {
+                button6.Text = "BOOM!";
+                button6.BackColor = Color.Red;
+            }
+            else
+            {
+                button6.Text = perem.ToString();
+                button6.BackColor = Color.Blue;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 3, 1);
+            if (perem == 7)
+            {
+                button8.Text = "BOOM!";
+                button8.BackColor = Color.Red;
+            }
+            else
+            {
+                button8.Text = perem.ToString();
+                button8.BackColor = Color.Blue;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Saper Saper = new Saper();
+            int perem = Saper.Sosed(polei, 3, 2);
+            if (perem == 7)
+            {
+                button9.Text = "BOOM!";
+                button9.BackColor = Color.Red;
+            }
+            else
+            {
+                button9.Text = perem.ToString();
+                button9.BackColor = Color.Blue;
+            }
+        }
+    }
+    public class Saper
+    {
+        public int[,] poley;
+       // = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+
+        public void saper(ref int[,] polei)
+        {
+            while (add(polei) < 3) { add(polei); }
+        }
+
+        public void newgame(ref int[,] polei)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    polei[i, j] = 0;
+                }
+            }
+        }
+        public int Sosed(int[,] polei, int a, int b)
+        {
+            int sosed = 0;
+            if (polei[a, b] != 8)
+            {
+                for (int i = a - 1; i < a + 2; i++)
+                {
+                    for (int j = b - 1; j < b + 2; j++)
+                    {
+                        if (polei[i, j] == 8)
+                        {
+                            sosed++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                sosed = 7;
+            }
+
+            return sosed;
+        }
+
+        int add(int[,] polei)
+        {
+            Random rnd = new Random();
+            int check = 0;
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    int q = rnd.Next(2);
+                    if (check >= 3)
+                    {
+                        break;
+                    }
+                    if (q == 1)
+                    {
+                        polei[i, j] = 8;
+                        check++;
+                    }
+                }
+            }
+            return check;
         }
     }
 }
